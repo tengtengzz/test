55e975d8bc48a71141f029eb0213da7e27baec56
wangshuangli <1107326411@qq.com>
2018-06-20 10:00:23 +0800

    Update 2.0.41.js

--- 2.0.41.js
+++ 2.0.41.js
@@ -1,27 +1,30 @@
-2.0.41|defineClass('MyMoeyManagementViewController', {
-    myMoeyManagementTableViewCell_button: function(cell, button) {
-        if (button.tag() == 1) {
+2.0.41|require('UIBarButtonItem,OneCoinMoneyChangeListViewController,BeginTranferMoneyViewController,GetMoneyViewController');
+defineClass('MyMoeyManagementViewController', {
+            myMoeyManagementTableViewCell_button: function(cell, button) {
+            if (button.tag() == 1) {
             //资金明细
             self.navigationController().navigationBar().setTranslucent(YES);
-            self.navigationController().navigationItem().backBarButtonItem().setBackButtonTitlePositionAdjustment_forBarMetrics(UIOffsetZero, UIBarMetricsDefault);\
-            var item = UIBarButtonItem.alloc().initWithTitle_style_target_action("明细", UIBarButtonItemStylePlain, null, null);\
+            self.navigationController().navigationItem().backBarButtonItem().setBackButtonTitlePositionAdjustment_forBarMetrics(UIOffsetZero, UIBarMetricsDefault);
+            var item = UIBarButtonItem.alloc().initWithTitle_style_target_action("明细", UIBarButtonItemStylePlain, null, null);
             self.navigationItem().setBackBarButtonItem(item);
             var vc = OneCoinMoneyChangeListViewController.alloc().init();
             vc.setSelectCoinType(cell.model().coinType().integerValue());
             self.navigationController().pushViewController_animated(vc, YES);
-        } else if (button.tag() == 3) {
+            } else if (button.tag() == 2) {
             //        充币
             var bibiOrderVC = BeginTranferMoneyViewController.alloc().init();
             bibiOrderVC.setSelectCoinType(cell.model().coinType().integerValue());
             self.navigationController().pushViewController_animated(bibiOrderVC, YES);
-        } else {
+            } else {
             //        提币m
             var vc = GetMoneyViewController.alloc().init();
             vc.setSelectCoinType(cell.model().coinType().integerValue());
             self.navigationController().pushViewController_animated(vc, YES);
+            
+            
+            }
+            
+            },
+            });
 
-
-        }
-
-    },
 });
