429a0171a3675bebaac29f89936b4e3bcd766522
wangshuangli <1107326411@qq.com>
2018-06-28 15:51:50 +0800

    Update and rename 2.0.71.js to 2.5.12.js

---
+++ 2.5.12.js
@@ -0,0 +1,19 @@
+require('OwnWebSocketUtil,WebSocketUtil');
+defineClass('BibiTransferInfoViewController', {
+    viewWillAppear: function(animated) {
+        self.super().viewWillAppear(animated);
+        if (self.isSelf()) {
+            OwnWebSocketUtil.sharedUtil().setDelegate(self);
+        } else {
+            WebSocketUtil.sharedUtil().setDelegate(self);
+        }
+
+        // 订阅
+        if (_hasSubscribed == NO) {
+            self.subscribeHuobiPanKou(YES);
+        }
+
+        self.updateData();
+        _convertLbl.setHidden(YES);
+    },
+});
