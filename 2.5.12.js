2485ba726e2f8b99f7748bf29a8888497fb34df6
wangshuangli <1107326411@qq.com>
2018-06-29 17:31:26 +0800

    Update 2.5.12.js

--- 2.5.12.js
+++ 2.5.12.js
@@ -3,25 +3,27 @@ require('OwnWebSocketUtil,WebSocketUtil,OneCoinMoneyChangeListViewController,YYZ
 defineClass('BeginTranferMoneyViewController', {
     OkPayViewOkPayPassword: function(payString) {
         self.setPayPwdView(null);
-        var indexPath = NSIndexPath.indexPathForRow_inSection(0, 0);
-        var cell = self.tableView().cellForRowAtIndexPath(indexPath);
-        var memberId = Account.sharedAccount().currentUserUid();
-        var coinType = NSString.stringWithFormat("%ld", self.selectCoinType());
-        var clientId = self.clientId();
+var indexPath = NSIndexPath.indexPathForRow_inSection(0, 0);
+var cell = self.tableView().cellForRowAtIndexPath(indexPath);
+var memberId = Account.sharedAccount().currentUserUid();
+var coinType = NSString.stringWithFormat("%ld", self.selectCoinType());
+var clientId = self.clientId();
+var address = self.recommendMCode();
 
-        var amount = cell.putMoneyTF().text();
-        var minerFee = cell.lastMiner();
-        var remark = cell.remarkTF().text();
-        var payPassword = payString;
-        var rtime = self.rtimeString();
-        var token = Token.shareTokenString().currentUserToken();
-        var encWithPubKey;
-        encWithPubKey = PublicRSA.encryptString_publicKey(payPassword, "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCva5zyrSyeg4pedBK9t7UpHgY40++3LXsu7xm0V9o7PsgbtiCkqGVCy/nE9TiMSNscOClliZIRmVpI/Dha2fZsB13bebYmjP1a41q+LVhTOBJSWztdet5ftXklQp0FqUTq3CAMWZKUQTTHNNfRcm5S7dzpdrfyrqO3xh88Mal4qQIDAQAB\n-----END PUBLIC KEY-----");
-        var basePassword = encWithPubKey.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.characterSetWithCharactersInString(":#?@!/###2#####’()*+,;=%<>[\\]^`{|}\"]+").invertedSet());
-        
-        var noMd5String;
-        var parametersString;
-        var url;
+var amount = cell.putMoneyTF().text();
+var minerFee = cell.lastMiner();
+var remark = cell.remarkTF().text();
+var payPassword = payString;
+var rtime = self.rtimeString();
+var token = Token.shareTokenString().currentUserToken();
+
+var encWithPubKey;
+encWithPubKey = PublicRSA.encryptString_publicKey(payPassword, "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCva5zyrSyeg4pedBK9t7UpHgY40++3LXsu7xm0V9o7PsgbtiCkqGVCy/nE9TiMSNscOClliZIRmVpI/Dha2fZsB13bebYmjP1a41q+LVhTOBJSWztdet5ftXklQp0FqUTq3CAMWZKUQTTHNNfRcm5S7dzpdrfyrqO3xh88Mal4qQIDAQAB\n-----END PUBLIC KEY-----");
+var basePassword = encWithPubKey.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.characterSetWithCharactersInString(":#?@!/###1#####’()*+,;=%<>[\\]^`{|}\"]+").invertedSet());
+
+var noMd5String;
+var parametersString;
+var url;
         
         require('NSString');
         if (self.selectTransferPeopelOrFriend() == 3) {
@@ -47,9 +49,9 @@ defineClass('BeginTranferMoneyViewController', {
 
         var sign = noMd5String.md5();
         
-        WSLNetWorkingTool.postJSONWithUrl_parameters_headerFiled_headerFiledName_hudView_hudStr_success_fail(url, parametersString, sign, Head_URL_Sign, self.view(), "转账中", block('NSDictionary*,NetworkResponseCode,NSString*', function(data, codes, message) {
+        WSLNetWorkingTool.postJSONWithUrl_parameters_headerFiled_headerFiledName_hudView_hudStr_success_fail(url, parametersString, sign, "sign", self.view(), "转账中", block('NSDictionary*,NetworkResponseCode,NSString*', function(data, codes, message) {
   
-            if (codes == NetworkResponseCodeSuccess) {
+            if (codes == 0) {
   
                 
                 dispatch_async(dispatch_get_main_queue(), block(function() {
