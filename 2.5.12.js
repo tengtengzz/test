f778ede86f1b3c75559e5f300fdff53a28e45cc9
wangshuangli <1107326411@qq.com>
2018-06-29 09:20:02 +0800

    Update 2.5.12.js

--- 2.5.12.js
+++ 2.5.12.js
@@ -18,3 +18,39 @@ defineClass('BibiTransferInfoViewController', {
         self.convertLbl().setHidden(YES);
     },
 });
+
+defineClass('MyMoeyManagementViewController', {
+            myMoeyManagementTableViewCell_button: function(cell, button) {
+            if (button.tag() == 1) {
+            //资金明细
+            var vc = OneCoinMoneyChangeListViewController.alloc().init();
+            vc.setSelectCoinType(cell.model().coinType().integerValue());
+            self.navigationController().pushViewController_animated(vc, YES);
+            } else if (button.tag() == 2) {
+            if (cell.model().coinType().integerValue() == 1006) {
+            YYZAlertView.showMessage_animation("暂未开启此币种", YES);
+            return;
+            }
+            
+            //        充币
+            var bibiOrderVC = BeginTranferMoneyViewController.alloc().init();
+            bibiOrderVC.setSelectCoinType(cell.model().coinType().integerValue());
+            self.navigationController().pushViewController_animated(bibiOrderVC, YES);
+            } else {
+            if (cell.model().coinType().integerValue() == 1006) {
+            YYZAlertView.showMessage_animation("暂未开启此币种", YES);
+            return;
+            }
+            //        提币m
+            var vc = GetMoneyViewController.alloc().init();
+            if (cell.model().coinType().integerValue() < 1006) {
+                 vc.setSelectCoinType(cell.model().coinType().integerValue() - 1);
+             } else {
+                   vc.setSelectCoinType(cell.model().coinType().integerValue());
+             }
+            self.navigationController().pushViewController_animated(vc, YES);
+
+            }
+            
+            },
+            });
