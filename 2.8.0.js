4459d9b29867c371583ec5d892bb4556ac185feb
wangshuangli <1107326411@qq.com>
2018-07-31 16:11:59 +0800

    Update and rename 2.7.5.js to 2.8.0.js

---
+++ 2.8.0.js
@@ -0,0 +1,8 @@
+
+defineClass('NSObject', {
+            decimalNumberjingduMaket: function(str) {
+            var ssss = self.decimalNumberjingdu(str);
+            return ssss;
+            
+            },
+            });
