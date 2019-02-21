a29737b6a81cc685f72033f1dbb841ba90fd374d
wangshuangli <1107326411@qq.com>
2019-02-21 18:17:44 +0800

    个人中心跳转

--- 20190214.js
+++ 20190214.js
@@ -1,4 +1,66 @@
-require('MerchantDetailTableViewCell,MerchantDetailViewController,NSString,NSInteger');
+require('MerchantDetailTableViewCell,MerchantDetailViewController,NSString,NSInteger,QNUploadManager,MBProgressHUD,UIImage,MyEduListViewController,MeLendingViewController,NewNeedPendingViewController,EGCommonWebViewController,NSIndexPath');
+
+defineClass('EGMeViewController', {
+            tableView_didSelectRowAtIndexPath: function(tableView, indexPath) {
+            
+            tableView.deselectRowAtIndexPath_animated(indexPath, YES);
+            var dic = self.dataArray().objectAtIndex(indexPath.section()).objectAtIndex(indexPath.row());
+            var index = dic.strValue("tag").integerValue();
+            switch (index) {
+            case 0:
+            {
+            self.navigationController().pushViewController_animated(MyEduListViewController.alloc().init(), YES);
+
+            }
+            break;
+            case 1:
+            {
+            self.navigationController().pushViewController_animated(MeLendingViewController.alloc().init(), YES);
+
+            }
+            break;
+            case 2:
+            {
+            self.navigationController().pushViewController_animated(NewNeedPendingViewController.alloc().init(), YES);
+
+            }
+            break;
+            case 3:
+            {
+            self.tabBarController().setSelectedIndex(1);
+
+            }
+            break;
+            case 4:
+            {
+            var web = EGCommonWebViewController.new();
+            web.setURL("http://www.yunshuidi.cn/html/help.html");
+            web.setTitle("问题详情");
+            self.navigationController().pushViewController_animated(web, YES);
+            }
+            break;
+            case 5:
+            {
+            var web = EGCommonWebViewController.new();
+            web.setURL("http://www.yunshuidi.cn/customer/index.html");
+            web.setTitle("客服热线");
+            self.navigationController().pushViewController_animated(web, YES);
+            }
+            break;
+            case 6:
+            {
+            var web = EGCommonWebViewController.new();
+            web.setURL("http://www.yunshuidi.cn/html/about.html");
+            web.setTitle("关于我们");
+            self.navigationController().pushViewController_animated(web, YES);
+            }
+            break;
+            default:
+            break;
+            }
+            },
+            });
+
 defineClass('TTInfoViewController', {
             merchantDetailTableViewCell_tag: function(cell, tag) {
             
