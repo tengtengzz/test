a72baff72ec9d3e75a894989f8e2c261366ff783
wangshuangli <1107326411@qq.com>
2019-02-25 14:41:27 +0800

    Update 2.8.7.js

--- 2.8.7.js
+++ 2.8.7.js
@@ -5,5 +5,7 @@ defineClass('TheMarketInfoListViewController', {
     if ([WebSocketUtil sharedUtil].wsState == SR_OPEN && _hasSubscribedHuobi == NO) {
         [self subscribeHuobiMarket:YES];
     }
+                self.isAddGestureLock().isHaveFriendRequest(); //是否有好友请求
+	        self.loadDataWithHudView_hudStr(null, null);
 
 });
