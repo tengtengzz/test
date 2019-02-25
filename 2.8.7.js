ff740f8433853b8605a161b3e1948eb5a51974fe
wangshuangli <1107326411@qq.com>
2019-02-25 14:27:57 +0800

    Update 2.8.7.js

--- 2.8.7.js
+++ 2.8.7.js
@@ -1,1 +1,9 @@
 
+defineClass('TheMarketInfoListViewController', {
+    viewWillAppear: function(animated) {
+        self.super().viewWillAppear(animated);
+    if ([WebSocketUtil sharedUtil].wsState == SR_OPEN && _hasSubscribedHuobi == NO) {
+        [self subscribeHuobiMarket:YES];
+    }
+
+});
