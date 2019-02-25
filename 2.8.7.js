e05613add989de7d3cec8db29f8500cde2fc5737
wangshuangli <1107326411@qq.com>
2019-02-25 14:42:52 +0800

    Update 2.8.7.js

--- 2.8.7.js
+++ 2.8.7.js
@@ -1,11 +1,47 @@
-
+require('UIBarButtonItem,UIImage,UIColor,WebSocketUtil');
 defineClass('TheMarketInfoListViewController', {
+    viewDidLoad: function() {
+        self.super().viewDidLoad();
+        self.navigationItem().setTitle(NSLocalizedString("行情", null));
+        var rightBarButtonItem = UIBarButtonItem.alloc().initWithImage_style_target_action(UIImage.imageNamed("navbar_refresh"), UIBarButtonItemStyleDone, self, @selector(refreshWebSocket));
+        self.navigationItem().setRightBarButtonItem(rightBarButtonItem);
+        self.view().setBackgroundColor(UIColor.whiteColor());
+        self.setMarket_close("1");
+        self.view().addSubview(self.topScroll());
+        self.view().addSubview(self.separatorLine());
+        self.view().addSubview(self.tableView());
+
+        self.requestCurrencyLists();
+        //    [self loadFloat];
+
+
+    },
     viewWillAppear: function(animated) {
         self.super().viewWillAppear(animated);
-    if ([WebSocketUtil sharedUtil].wsState == SR_OPEN && _hasSubscribedHuobi == NO) {
-        [self subscribeHuobiMarket:YES];
-    }
-                self.isAddGestureLock().isHaveFriendRequest(); //是否有好友请求
-	        self.loadDataWithHudView_hudStr(null, null);
+        WebSocketUtil.sharedUtil().setDelegate(self);
+        //    [OwnWebSocketUtil sharedUtil].delegate() = self;
+        // 订阅
+        if (WebSocketUtil.sharedUtil().wsState() == SR_OPEN && _hasSubscribedHuobi == NO) {
+            self.subscribeHuobiMarket(YES);
+        }
 
+        //    if ([OwnWebSocketUtil sharedUtil].wsState() == SR_OPEN && _hasSubscribedBibao == NO) {
+        //        [self subscribeBibaoMarket:NO];
+        //    }
+    },
+    viewWillDisappear: function(animated) {
+        self.super().viewWillDisappear(animated);
+        if (WebSocketUtil.sharedUtil().delegate() == self) WebSocketUtil.sharedUtil().setDelegate(null);
+        //    if ([OwnWebSocketUtil sharedUtil].delegate() == self) [OwnWebSocketUtil sharedUtil].delegate() = null;
+        // 取消订阅
+        if (_hasSubscribedHuobi) {
+            self.subscribeHuobiMarket(NO);
+        }
+        //    if (_hasSubscribedBibao) {
+        //        [self subscribeBibaoMarket:NO];
+        //    }
+
+
+        !self.viewWillDisappearBlock() ? : self.viewWillDisappearBlock()();
+    },
 });
